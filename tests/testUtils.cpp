e5e425fbc571df3b081a18d14ee4ab4a17127d55
adthoms <adthoms@uwaterloo.ca>
2024-01-03 18:39:33 -0800

    add SE and RA matrix partition and creation unit tests

--- tests/testUtils.cpp
+++ tests/testUtils.cpp
@@ -81,3 +81,119 @@ TEST(testDCORA, testChi2Inv) {
   double q = (double) count / numTrials;
   ASSERT_LE(abs(q - quantile), 0.01);
 }
+
+TEST(testDCORA, testPartitionSEMatrix) {
+  int d = 3;
+  int r = 5;
+  int n = 3;
+  Matrix Y1 = randomStiefelVariable(r, d);
+  Matrix Y2 = randomStiefelVariable(r, d);
+  Matrix Y3 = randomStiefelVariable(r, d);
+  Matrix p1 = randomEuclideanVariable(r);
+  Matrix p2 = randomEuclideanVariable(r);
+  Matrix p3 = randomEuclideanVariable(r);
+  Matrix X_SE(r, (d + 1) * n);
+  Matrix X_SE_R(r, d * n);
+  Matrix X_SE_t(r, n);
+  X_SE << Y1, p1, Y2, p2, Y3, p3;
+  X_SE_R << Y1, Y2, Y3;
+  X_SE_t << p1, p2, p3;
+  auto [var_SE_R, var_SE_t] = partitionSEMatrix(X_SE, r, d, n);
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < d * n; ++j) {
+      ASSERT_EQ(var_SE_R(i, j), X_SE_R(i, j));
+    }
+    for (int j = 0; j < n; ++j) {
+      ASSERT_EQ(var_SE_t(i, j), X_SE_t(i, j));
+    }
+  }
+}
+
+TEST(testDCORA, testPartitionRAMatrix) {
+  int d = 3;
+  int r = 5;
+  int n = 3;
+  int l = 5;
+  int b = 7;
+  Matrix Y1 = randomStiefelVariable(r, d);
+  Matrix Y2 = randomStiefelVariable(r, d);
+  Matrix Y3 = randomStiefelVariable(r, d);
+  Matrix p1 = randomEuclideanVariable(r);
+  Matrix p2 = randomEuclideanVariable(r);
+  Matrix p3 = randomEuclideanVariable(r);
+  Matrix X_OB = randomObliqueVariable(r, l);
+  Matrix X_E = randomEuclideanVariable(r, b);
+  Matrix X_RA(r, (d + 1) * n + l + b);
+  Matrix X_SE_R(r, d * n);
+  Matrix X_SE_t(r, n);
+  X_RA << Y1, Y2, Y3, X_OB, p1, p2, p3, X_E;
+  X_SE_R << Y1, Y2, Y3;
+  X_SE_t << p1, p2, p3;
+  auto [var_SE_R, var_OB, var_SE_t, var_E] = partitionRAMatrix(X_RA, r, d, n, l, b);
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < d * n; ++j) {
+      ASSERT_EQ(var_SE_R(i, j), X_SE_R(i, j));
+    }
+    for (int j = 0; j < l; ++j) {
+      ASSERT_EQ(var_OB(i, j), X_OB(i, j));
+    }
+    for (int j = 0; j < n; ++j) {
+      ASSERT_EQ(var_SE_t(i, j), X_SE_t(i, j));
+    }
+    for (int j = 0; j < b; ++j) {
+      ASSERT_EQ(var_E(i, j), X_E(i, j));
+    }
+  }
+}
+
+TEST(testDCORA, testCreateSEMatrix) {
+  int d = 3;
+  int r = 5;
+  int n = 3;
+  Matrix Y1 = randomStiefelVariable(r, d);
+  Matrix Y2 = randomStiefelVariable(r, d);
+  Matrix Y3 = randomStiefelVariable(r, d);
+  Matrix p1 = randomEuclideanVariable(r);
+  Matrix p2 = randomEuclideanVariable(r);
+  Matrix p3 = randomEuclideanVariable(r);
+  Matrix X_SE(r, (d + 1) * n);
+  Matrix X_SE_R(r, d * n);
+  Matrix X_SE_t(r, n);
+  X_SE << Y1, p1, Y2, p2, Y3, p3;
+  X_SE_R << Y1, Y2, Y3;
+  X_SE_t << p1, p2, p3;
+  Matrix var_X_SE = createSEMatrix(X_SE_R, X_SE_t);
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < (d + 1) * n; ++j) {
+      ASSERT_EQ(var_X_SE(i, j), X_SE(i, j));
+    }
+  }
+}
+
+TEST(testDCORA, testCreateRAMatrix) {
+  int d = 3;
+  int r = 5;
+  int n = 3;
+  int l = 5;
+  int b = 7;
+  Matrix Y1 = randomStiefelVariable(r, d);
+  Matrix Y2 = randomStiefelVariable(r, d);
+  Matrix Y3 = randomStiefelVariable(r, d);
+  Matrix p1 = randomEuclideanVariable(r);
+  Matrix p2 = randomEuclideanVariable(r);
+  Matrix p3 = randomEuclideanVariable(r);
+  Matrix X_OB = randomObliqueVariable(r, l);
+  Matrix X_E = randomEuclideanVariable(r, b);
+  Matrix X_RA(r, (d + 1) * n + l + b);
+  Matrix X_SE_R(r, d * n);
+  Matrix X_SE_t(r, n);
+  X_RA << Y1, Y2, Y3, X_OB, p1, p2, p3, X_E;
+  X_SE_R << Y1, Y2, Y3;
+  X_SE_t << p1, p2, p3;
+  Matrix var_X_RA = createRAMatrix(X_SE_R, X_OB, X_SE_t, X_E);
+  for (int i = 0; i < r; ++i) {
+    for (int j = 0; j < (d + 1) * n + l + b; ++j) {
+      ASSERT_EQ(var_X_RA(i, j), X_RA(i, j));
+    }
+  }
+}
