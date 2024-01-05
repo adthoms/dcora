fad8347bd7c4d01c7fc1f9bc07e9be7bf74ff0b6
adthoms <adthoms@uwaterloo.ca>
2024-01-05 15:17:46 -0800

    reformat tests

--- tests/testUtils.cpp
+++ tests/testUtils.cpp
@@ -1,26 +1,25 @@
-#include <DCORA/DCORA_types.h>
-#include <DCORA/DCORA_solver.h>
 #include <DCORA/DCORA_robust.h>
+#include <DCORA/DCORA_solver.h>
+#include <DCORA/DCORA_types.h>
 #include <DCORA/PoseGraph.h>
 #include <DCORA/manifold/LiftedSEManifold.h>
+
 #include <iostream>
 #include <random>
 
 #include "gtest/gtest.h"
 
-using namespace DCORA;
-
 TEST(testDCORA, testStiefelGeneration) {
-  Matrix Y = fixedStiefelVariable(5, 3);
-  Matrix I = Matrix::Identity(3, 3);
-  Matrix D = Y.transpose() * Y - I;
+  DCORA::Matrix Y = DCORA::fixedStiefelVariable(5, 3);
+  DCORA::Matrix I = DCORA::Matrix::Identity(3, 3);
+  DCORA::Matrix D = Y.transpose() * Y - I;
   ASSERT_LE(D.norm(), 1e-5);
 }
 
 TEST(testDCORA, testStiefelRepeat) {
-  Matrix Y = fixedStiefelVariable(5, 3);
+  DCORA::Matrix Y = DCORA::fixedStiefelVariable(5, 3);
   for (size_t i = 0; i < 10; ++i) {
-    Matrix Y_ = fixedStiefelVariable(5, 3);
+    DCORA::Matrix Y_ = DCORA::fixedStiefelVariable(5, 3);
     ASSERT_LE((Y_ - Y).norm(), 1e-5);
   }
 }
@@ -28,11 +27,11 @@ TEST(testDCORA, testStiefelRepeat) {
 TEST(testDCORA, testStiefelProjection) {
   size_t d = 3;
   size_t r = 5;
-  Matrix I = Matrix::Identity(d, d);
+  DCORA::Matrix I = DCORA::Matrix::Identity(d, d);
   for (size_t j = 0; j < 50; ++j) {
-    Matrix M = Matrix::Random(r, d);
-    Matrix Y = projectToStiefelManifold(M);
-    Matrix D = Y.transpose() * Y - I;
+    DCORA::Matrix M = DCORA::Matrix::Random(r, d);
+    DCORA::Matrix Y = DCORA::projectToStiefelManifold(M);
+    DCORA::Matrix D = Y.transpose() * Y - I;
     ASSERT_LE(D.norm(), 1e-5);
   }
 }
@@ -40,11 +39,11 @@ TEST(testDCORA, testStiefelProjection) {
 TEST(testDCORA, testObliqueProjection) {
   size_t d = 3;
   size_t r = 5;
-  Matrix I = Matrix::Identity(d, d);
+  DCORA::Matrix I = DCORA::Matrix::Identity(d, d);
   for (size_t j = 0; j < 50; ++j) {
-    Matrix M = Matrix::Random(r, d);
-    Matrix Y = projectToObliqueManifold(M);
-    Matrix D = (Y.transpose() * Y).diagonal() - I.diagonal();
+    DCORA::Matrix M = DCORA::Matrix::Random(r, d);
+    DCORA::Matrix Y = DCORA::projectToObliqueManifold(M);
+    DCORA::Matrix D = (Y.transpose() * Y).diagonal() - I.diagonal();
     ASSERT_LE(D.norm(), 1e-5);
   }
 }
@@ -53,14 +52,14 @@ TEST(testDCORA, testLiftedSEManifoldProjection) {
   int d = 3;
   int r = 5;
   int n = 100;
-  LiftedSEManifold Manifold(r, d, n);
-  Matrix M = Matrix::Random(r, (d + 1) * n);
-  Matrix X = Manifold.project(M);
+  DCORA::LiftedSEManifold Manifold(r, d, n);
+  DCORA::Matrix M = DCORA::Matrix::Random(r, (d + 1) * n);
+  DCORA::Matrix X = Manifold.project(M);
   ASSERT_EQ(X.rows(), r);
   ASSERT_EQ(X.cols(), (d + 1) * n);
   for (int i = 0; i < n; ++i) {
-    Matrix Y = X.block(0, i * (d + 1), r, d);
-    Matrix D = Y.transpose() * Y - Matrix::Identity(d, d);
+    DCORA::Matrix Y = X.block(0, i * (d + 1), r, d);
+    DCORA::Matrix D = Y.transpose() * Y - DCORA::Matrix::Identity(d, d);
     ASSERT_LE(D.norm(), 1e-5);
   }
 }
@@ -68,17 +67,18 @@ TEST(testDCORA, testLiftedSEManifoldProjection) {
 TEST(testDCORA, testChi2Inv) {
   unsigned dof = 4;
   double quantile = 0.95;
-  double threshold = chi2inv(quantile, dof);
-  std::random_device rd;  // Will be used to obtain a seed for the random number engine
-  std::mt19937 rng(rd());  // Standard mersenne_twister_engine seeded with rd()
+  double threshold = DCORA::chi2inv(quantile, dof);
+  std::random_device rd;  // obtain a seed for the random number engine
+  std::mt19937 rng(rd()); // Standard mersenne_twister_engine seeded with rd()
   std::chi_squared_distribution<double> distribution(dof);
   int numTrials = 100000;
   int count = 0;
   for (int i = 0; i < numTrials; ++i) {
     double number = distribution(rng);
-    if (number < threshold) count++;
+    if (number < threshold)
+      count++;
   }
-  double q = (double) count / numTrials;
+  double q = static_cast<double>(count) / numTrials;
   ASSERT_LE(abs(q - quantile), 0.01);
 }
 
@@ -86,19 +86,19 @@ TEST(testDCORA, testPartitionSEMatrix) {
   int d = 3;
   int r = 5;
   int n = 3;
-  Matrix Y1 = randomStiefelVariable(r, d);
-  Matrix Y2 = randomStiefelVariable(r, d);
-  Matrix Y3 = randomStiefelVariable(r, d);
-  Matrix p1 = randomEuclideanVariable(r);
-  Matrix p2 = randomEuclideanVariable(r);
-  Matrix p3 = randomEuclideanVariable(r);
-  Matrix X_SE(r, (d + 1) * n);
-  Matrix X_SE_R(r, d * n);
-  Matrix X_SE_t(r, n);
-  X_SE << Y1, p1, Y2, p2, Y3, p3;
-  X_SE_R << Y1, Y2, Y3;
-  X_SE_t << p1, p2, p3;
-  auto [var_SE_R, var_SE_t] = partitionSEMatrix(X_SE, r, d, n);
+  DCORA::Matrix Y0 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y1 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y2 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix p0 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p1 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p2 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix X_SE(r, (d + 1) * n);
+  DCORA::Matrix X_SE_R(r, d * n);
+  DCORA::Matrix X_SE_t(r, n);
+  X_SE << Y0, p0, Y1, p1, Y2, p2;
+  X_SE_R << Y0, Y1, Y2;
+  X_SE_t << p0, p1, p2;
+  auto [var_SE_R, var_SE_t] = DCORA::partitionSEMatrix(X_SE, r, d, n);
   for (int i = 0; i < r; ++i) {
     for (int j = 0; j < d * n; ++j) {
       ASSERT_EQ(var_SE_R(i, j), X_SE_R(i, j));
@@ -115,21 +115,22 @@ TEST(testDCORA, testPartitionRAMatrix) {
   int n = 3;
   int l = 5;
   int b = 7;
-  Matrix Y1 = randomStiefelVariable(r, d);
-  Matrix Y2 = randomStiefelVariable(r, d);
-  Matrix Y3 = randomStiefelVariable(r, d);
-  Matrix p1 = randomEuclideanVariable(r);
-  Matrix p2 = randomEuclideanVariable(r);
-  Matrix p3 = randomEuclideanVariable(r);
-  Matrix X_OB = randomObliqueVariable(r, l);
-  Matrix X_E = randomEuclideanVariable(r, b);
-  Matrix X_RA(r, (d + 1) * n + l + b);
-  Matrix X_SE_R(r, d * n);
-  Matrix X_SE_t(r, n);
-  X_RA << Y1, Y2, Y3, X_OB, p1, p2, p3, X_E;
-  X_SE_R << Y1, Y2, Y3;
-  X_SE_t << p1, p2, p3;
-  auto [var_SE_R, var_OB, var_SE_t, var_E] = partitionRAMatrix(X_RA, r, d, n, l, b);
+  DCORA::Matrix Y0 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y1 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y2 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix p0 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p1 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p2 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix X_OB = DCORA::randomObliqueVariable(r, l);
+  DCORA::Matrix X_E = DCORA::randomEuclideanVariable(r, b);
+  DCORA::Matrix X_RA(r, (d + 1) * n + l + b);
+  DCORA::Matrix X_SE_R(r, d * n);
+  DCORA::Matrix X_SE_t(r, n);
+  X_RA << Y0, Y1, Y2, X_OB, p0, p1, p2, X_E;
+  X_SE_R << Y0, Y1, Y2;
+  X_SE_t << p0, p1, p2;
+  auto [var_SE_R, var_OB, var_SE_t, var_E] =
+      DCORA::partitionRAMatrix(X_RA, r, d, n, l, b);
   for (int i = 0; i < r; ++i) {
     for (int j = 0; j < d * n; ++j) {
       ASSERT_EQ(var_SE_R(i, j), X_SE_R(i, j));
@@ -150,19 +151,19 @@ TEST(testDCORA, testCreateSEMatrix) {
   int d = 3;
   int r = 5;
   int n = 3;
-  Matrix Y1 = randomStiefelVariable(r, d);
-  Matrix Y2 = randomStiefelVariable(r, d);
-  Matrix Y3 = randomStiefelVariable(r, d);
-  Matrix p1 = randomEuclideanVariable(r);
-  Matrix p2 = randomEuclideanVariable(r);
-  Matrix p3 = randomEuclideanVariable(r);
-  Matrix X_SE(r, (d + 1) * n);
-  Matrix X_SE_R(r, d * n);
-  Matrix X_SE_t(r, n);
-  X_SE << Y1, p1, Y2, p2, Y3, p3;
-  X_SE_R << Y1, Y2, Y3;
-  X_SE_t << p1, p2, p3;
-  Matrix var_X_SE = createSEMatrix(X_SE_R, X_SE_t);
+  DCORA::Matrix Y0 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y1 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y2 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix p0 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p1 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p2 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix X_SE(r, (d + 1) * n);
+  DCORA::Matrix X_SE_R(r, d * n);
+  DCORA::Matrix X_SE_t(r, n);
+  X_SE << Y0, p0, Y1, p1, Y2, p2;
+  X_SE_R << Y0, Y1, Y2;
+  X_SE_t << p0, p1, p2;
+  DCORA::Matrix var_X_SE = DCORA::createSEMatrix(X_SE_R, X_SE_t);
   for (int i = 0; i < r; ++i) {
     for (int j = 0; j < (d + 1) * n; ++j) {
       ASSERT_EQ(var_X_SE(i, j), X_SE(i, j));
@@ -176,21 +177,21 @@ TEST(testDCORA, testCreateRAMatrix) {
   int n = 3;
   int l = 5;
   int b = 7;
-  Matrix Y1 = randomStiefelVariable(r, d);
-  Matrix Y2 = randomStiefelVariable(r, d);
-  Matrix Y3 = randomStiefelVariable(r, d);
-  Matrix p1 = randomEuclideanVariable(r);
-  Matrix p2 = randomEuclideanVariable(r);
-  Matrix p3 = randomEuclideanVariable(r);
-  Matrix X_OB = randomObliqueVariable(r, l);
-  Matrix X_E = randomEuclideanVariable(r, b);
-  Matrix X_RA(r, (d + 1) * n + l + b);
-  Matrix X_SE_R(r, d * n);
-  Matrix X_SE_t(r, n);
-  X_RA << Y1, Y2, Y3, X_OB, p1, p2, p3, X_E;
-  X_SE_R << Y1, Y2, Y3;
-  X_SE_t << p1, p2, p3;
-  Matrix var_X_RA = createRAMatrix(X_SE_R, X_OB, X_SE_t, X_E);
+  DCORA::Matrix Y0 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y1 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix Y2 = DCORA::randomStiefelVariable(r, d);
+  DCORA::Matrix p0 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p1 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix p2 = DCORA::randomEuclideanVariable(r);
+  DCORA::Matrix X_OB = DCORA::randomObliqueVariable(r, l);
+  DCORA::Matrix X_E = DCORA::randomEuclideanVariable(r, b);
+  DCORA::Matrix X_RA(r, (d + 1) * n + l + b);
+  DCORA::Matrix X_SE_R(r, d * n);
+  DCORA::Matrix X_SE_t(r, n);
+  X_RA << Y0, Y1, Y2, X_OB, p0, p1, p2, X_E;
+  X_SE_R << Y0, Y1, Y2;
+  X_SE_t << p0, p1, p2;
+  DCORA::Matrix var_X_RA = DCORA::createRAMatrix(X_SE_R, X_OB, X_SE_t, X_E);
   for (int i = 0; i < r; ++i) {
     for (int j = 0; j < (d + 1) * n + l + b; ++j) {
       ASSERT_EQ(var_X_RA(i, j), X_RA(i, j));
