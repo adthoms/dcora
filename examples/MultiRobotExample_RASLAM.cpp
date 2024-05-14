95200f33cb0816e7b840edcdfd369459f75b4996
adthoms <adthoms@uwaterloo.ca>
2024-05-14 13:00:31 -0700

    add initial single- and multi-robot RA-SLAM examples for PyFG i/o testing

---
+++ examples/MultiRobotExample_RASLAM.cpp
@@ -0,0 +1,42 @@
+/* -----------------------------------------------------------------------------
+ * Copyright 2020, Massachusetts Institute of Technology, * Cambridge, MA 02139
+ * Copyright 2024, University of California Los Angeles, * Los Angeles, CA 90095
+ * All Rights Reserved
+ * Authors: Yulun Tian, Alexander Thoms, Alan Papalia, et al.
+ *  - For dpgo's full author list, see:
+ *  https://github.com/mit-acl/dpgo/blob/main/README.md
+ *  - For dcora's full author list, see dcora/README.md
+ * See LICENSE for the license information
+ * -------------------------------------------------------------------------- */
+
+#include <DCORA/Agent.h>
+#include <DCORA/DCORA_solver.h>
+#include <DCORA/DCORA_types.h>
+#include <DCORA/QuadraticProblem.h>
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  /**
+  ###########################################
+  Parse input dataset
+  ###########################################
+  */
+
+  if (argc < 2) {
+    std::cout << "Multi-robot RA-SLAM demo. " << std::endl;
+    std::cout << "Usage: " << argv[0] << " [input .pyfg file]" << std::endl;
+    exit(1);
+  }
+
+  std::cout << "Multi-robot RA-SLAM demo. " << std::endl;
+
+  DCORA::PyFGDataset dataset = DCORA::read_pyfg_file(argv[1]);
+
+  // TODO(Alex): Implement remaining RA-SLAM demo similar to
+  // MultiRobotExample.cpp
+
+  exit(0);
+}
